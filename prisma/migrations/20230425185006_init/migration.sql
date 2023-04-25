e6e0d4638da7275985930805dca7d97c8417c155
WaguinhoCarvalho <waguinho.carvalho@hotmail.com>
2023-04-25 17:49:24 -0300

    Initial commit - Structure login initial

---
+++ prisma/migrations/20230425185006_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashPassword" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "disabledAt" TIMESTAMP(3)
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_key" ON "user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
---
+++ src/helpers/constants.ts
@@ -0,0 +1,1 @@
+export const jwtSecretEnv = process.env.JWT_SECRET;
